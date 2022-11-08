ceab99f3d55372381a91967ca054f0f57fcf1364
MichaHornung <michaelhornung2022@icloud.com>
2022-11-08 15:19:48 +0100

    aufgäbe erledigt

---
+++ TableView DataSource/TableView DataSource/TeamsTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TeamsTableViewCell.swift
+//  TableView DataSource
+//
+//  Created by Michael Hornung on 08.11.22.
+//
+
+import UIKit
+
+class TeamsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var countryLabel: UILabel!
+    
+    @IBOutlet weak var flagimage: UIImageView!
+    
+
+}
